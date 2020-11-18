62c7cef979f6bfd3df25120ce9b6c50c1a97e39f
Yulia-Yakovleva <st041958@student.spbu.ru>
2020-11-18 14:54:35 +0300

    add habitat distribution

---
+++ src/countplot.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set(style='darkgrid')
+width=10
+height=6
+plt.figure(figsize=(width,height))
+
+df = pd.read_csv('/home/yulia/micrometa/metadata/habitat_data.tsv', sep='\t')
+df = df[df.habitat != 'metagenome']
+
+sns.countplot(x='type', hue='habitat', data=df, palette="Set3")
+plt.title('Founded sequences in different habitats')
+plt.xlabel('Ribosomal genes')
+plt.ylabel('Counts')
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.tight_layout()
+plt.savefig('/home/yulia/micrometa/figures/habitats_countplot_meta.pdf', format='pdf')
+plt.savefig('/home/yulia/micrometa/figures/habitats_countplot_meta.png', format='png', dpi=600)
+plt.show()
--- src/create_habitat_metadata.py
+++ src/create_habitat_metadata.py
@@ -1,1 +1,26 @@
- 
+import glob
+import pandas as pd
+from subprocess import Popen, PIPE
+
+pth = '/home/yulia/micrometa/metadata/'
+
+df = pd.DataFrame(columns=['seq_name', 'type', 'habitat'])
+
+for filename in glob.glob(pth+'*.lst'):
+    stdout, stderr = Popen(["wc", "-l", filename], stdout=PIPE).communicate()
+    file_length = stdout.decode("utf-8").split(' ')[0]
+    habitat = filename.split('/')[-1].replace('.lst', '')
+    print(f'Working with habitat {habitat}...')
+    with open(filename, 'r') as in_f:
+        for count, line in enumerate(in_f):
+            seq_name = line.strip().replace('>', '')
+            gene = seq_name.split('_rRNA')[0]
+            print(f'Working with habitat {habitat}...\t{count} / {file_length}', end='\r')
+            #print(f"{seq_name}\t{gene}\t{habitat}")
+            df = df.append({'seq_name': seq_name,
+                            'type': gene,
+                            'habitat': habitat},
+            ignore_index=True)        
+
+print(df)
+df.to_csv(pth+'habitat_info.tsv', sep='\t')
